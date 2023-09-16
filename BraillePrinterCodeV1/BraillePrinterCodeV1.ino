d4647bc393a7cdaade31a656647b148006191996
Marie De Mey <97183312+mariedemeyy@users.noreply.github.com>
2023-09-16 18:30:49 -0400

    Mirror braille for punching
    - Select letters that commonly occur in the English alphabet
    - Change logic to mirror braille
    - Implement Serial print code to print the mirrored braille

--- BraillePrinterCodeV1/BraillePrinterCodeV1.ino
+++ BraillePrinterCodeV1/BraillePrinterCodeV1.ino
@@ -6,25 +6,25 @@ String input;
 
 arx::map<char, String> mp {
   {'a', "100000"},
-  {'b', "110000"},
-  {'c', "100100"},
-  {'d', "100110"},
+  // {'b', "110000"},
+  // {'c', "100100"},
+  // {'d', "100110"},
   {'e', "100010"},
-  {'f', "110100"},
-  {'g', "110110"},
+  // {'f', "110100"},
+  // {'g', "110110"},
   {'h', "110010"},
-  {'i', "010100"}
+  // {'i', "010100"},
   // {'j', "010110"},
   // {'k', "101000"},
   // {'l', "111000"},
   // {'m', "101100"},
   // {'n', "101110"},
-  // {'o', "101000"},
+  {'o', "101000"},
   // {'p', "111100"},
   // {'q', "111110"},
-  // {'r', "111010"},
-  // {'s', "011100"},
-  // {'t', "011110"},
+  {'r', "111010"},
+  {'s', "011100"},
+  {'t', "011110"}
   // {'u', "101001"},
   // {'v', "111001"},
   // {'w', "010111"},
@@ -35,7 +35,8 @@ arx::map<char, String> mp {
 
 void setup() {
     Serial.begin(9600); 
- 
+    Serial.println("");
+    delay(1000);
     Serial.println("Type something!");
 }
 
@@ -59,55 +60,103 @@ void loop() {
       char topRow[10]; 
       char middleRow[10];
       char bottomRow[10];
-      int leftCol;
-      int rightCol;
-      
+      int leftCol = 0;
+      int rightCol = 1;
+      int incrementor = 2;
 
+      Serial.println(sizeof(WordArray));
+      Serial.println(WordLength);
+      Serial.println("debug word");
+      for(int i = 0; i < WordLength - 1; i++) {
+        Serial.print(WordArray[i]);
+      }
+      Serial.println("");
       // ISSUE WITH HOW IT PRINTS
       // TRYING WITH ABE
       // IT NEEDS TO PRINT E B A 
-      for(int i = WordLength - 1; i >= 0; i--)
+      for(int i = WordLength - 2; i >= 0; i--)
       {
         // Get the Braille code for the letter in the array
+        Serial.println(leftCol);
+        Serial.println(rightCol);
         Serial.print("letter: ");
         Serial.println(WordArray[i]); 
 
         String brailleCode = mp[WordArray[i]];
         Serial.print("try with char: ");
         Serial.println(brailleCode);
-       
-        for(int j = 0; j < WordLength - 1; j++) {
+
+        topRow[leftCol] = brailleCode.charAt(3);
+        topRow[rightCol] = brailleCode.charAt(0);
         
-          leftCol = j * 2;
-          rightCol = (j * 2) + 1;
+        middleRow[leftCol] = brailleCode.charAt(4);
+        middleRow[rightCol] = brailleCode.charAt(1);
 
-          // Get 0 and 3 for topRow
-          topRow[leftCol] = brailleCode.charAt(3);
-          topRow[rightCol] = brailleCode.charAt(0);
+        bottomRow[leftCol] = brailleCode.charAt(5);
+        bottomRow[rightCol] = brailleCode.charAt(2);
+        
+        leftCol += incrementor;
+        rightCol += incrementor;
+        // for(int j = 0; j < WordLength*2; j += incrementor) {
+        //   Serial.println(j);
+        //   leftCol = j;
+        //   rightCol = j + 1;
 
-          // Get 1 and 4 for middleRow
-          middleRow[leftCol] = brailleCode.charAt(4);
-          middleRow[rightCol] = brailleCode.charAt(1);
+        //   // Get 0 and 3 for topRow
+        //   topRow[leftCol] = brailleCode.charAt(3);
+        //   topRow[rightCol] = brailleCode.charAt(0);
 
-          // Get 2 and 5 for bottomRow
-          bottomRow[leftCol] = brailleCode.charAt(5);
-          bottomRow[rightCol] = brailleCode.charAt(2);
+        //   // Get 1 and 4 for middleRow
+        //   middleRow[leftCol] = brailleCode.charAt(4);
+        //   middleRow[rightCol] = brailleCode.charAt(1);
 
-        }
+        //   // Get 2 and 5 for bottomRow
+        //   bottomRow[leftCol] = brailleCode.charAt(5);
+        //   bottomRow[rightCol] = brailleCode.charAt(2);
+
+        // }
       }
       // MOTOR STUFF, USE THE ARRAYS
       Serial.println("topRow");
       for(int i = 0; i < 10; i++) {
         Serial.print(topRow[i]);
       }
+
+      Serial.println("");
       Serial.println("middleRow");
       for(int i = 0; i < 10; i++) {
         Serial.print(middleRow[i]);
       }
+
+      Serial.println("");
       Serial.println("bottomRow");
       for(int i = 0; i < 10; i++) {
         Serial.print(bottomRow[i]);
       }
-    }
-    
+
+      Serial.println("");
+      for(int i = 0; i < 10; i++) {
+        if(topRow[i] == '1') {
+          Serial.print('.');
+        } else {
+          Serial.print(' ');
+        }
+      }
+      Serial.println("");
+      for(int i = 0; i < 10; i++) {
+        if(middleRow[i] == '1') {
+          Serial.print('.');
+        } else {
+          Serial.print(' ');
+        }
+      }
+      Serial.println("");
+      for(int i = 0; i < 10; i++) {
+        if(bottomRow[i] == '1') {
+          Serial.print('.');
+        } else {
+          Serial.print(' ');
+        }
+      }
+    } 
 }
