3a7b1ab897b23cc2cc15801326a88e81f3b919e0
Marie De Mey <97183312+mariedemeyy@users.noreply.github.com>
2023-09-16 13:39:01 -0400

    Add entire dictionary

--- BraillePrinterCodeV1/BraillePrinterCodeV1.ino
+++ BraillePrinterCodeV1/BraillePrinterCodeV1.ino
@@ -1,6 +1,35 @@
 #include <ArxContainer.h>
 String input;
- 
+
+arx::map<String, String> mp {
+  {"a", "100000"},
+  {"b", "110000"},
+  {"c", "100100"},
+  {"d", "100110"},
+  {"e", "100010"},
+  {"f", "110100"},
+  {"g", "110110"},
+  {"h", "110010"},
+  {"i", "010100"},
+  {"j", "010110"},
+  {"k", "101000"},
+  {"l", "111000"},
+  {"m", "101100"},
+  {"n", "101110"},
+  {"o", "101000"},
+  {"p", "111100"},
+  {"q", "111110"},
+  {"r", "111010"},
+  {"s", "011100"},
+  {"t", "011110"},
+  {"u", "101001"},
+  {"v", "111001"},
+  {"w", "010111"},
+  {"x", "101101"},
+  {"y", "101111"},
+  {"z", "101011"}
+};
+
 void setup() {
     Serial.begin(9600); 
     delay(2000);  
@@ -20,30 +49,32 @@ void loop() {
       char WordArray[WordLength]; 
       input.toCharArray(WordArray, WordLength); 
       
-      arx::map<String, String> mp {
-      {"a", "100000"}, 
-      {"b", "110000"}
-      };
 
       // Print each element of the character array OR
       // do something with the character array
-      for(int i = 0; i < WordLength; i++)
+      for(int i = 0; i < WordLength - 1; i++)
       {
-        // Compare the letter to the dictionary
-        Serial.println("hello");
-        for (const auto& m : mp)
-        {
-          if(m.first == WordArray[i]) {
-            Serial.println("True");
-          }
-          Serial.println(mp["a"]);
-          // Serial.print("{");
-          // Serial.print(m.first); Serial.print(",");
-          // Serial.print(m.second);
-          // Serial.println("}");
-        }
-        
+        // Get the Braille code for the letter in the array
+
+        Serial.print("letter: ");
         Serial.println(WordArray[i]); 
+        Serial.print("braille code: ");
+        Serial.println(String(WordArray[i])); 
+        Serial.println(mp[String(WordArray[i])]);
+
+        // Serial.println("hello");
+        // for (const auto& m : mp)
+        // {
+        //   if(m.first == WordArray[i]) {
+        //     Serial.println("True");
+        //   }
+        //   Serial.println(mp["a"]);
+        //   // Serial.print("{");
+        //   // Serial.print(m.first); Serial.print(",");
+        //   // Serial.print(m.second);
+        //   // Serial.println("}");
+        // }
+        
       }
     }
     
