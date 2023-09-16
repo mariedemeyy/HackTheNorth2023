3f66dcbfd518d906e676a36787fd8eaa1a678abe
Marie De Mey <97183312+mariedemeyy@users.noreply.github.com>
2023-09-16 13:00:41 -0400

    Add ArxContainer library
    - Create a map of 2 braille letters (so far)
    - Iterate through it and print

--- BraillePrinterCodeV1/BraillePrinterCodeV1.ino
+++ BraillePrinterCodeV1/BraillePrinterCodeV1.ino
@@ -1,3 +1,4 @@
+#include <ArxContainer.h>
 String input;
  
 void setup() {
@@ -6,16 +7,16 @@ void setup() {
  
     Serial.println("Type something!");
 }
- 
+
+
 void loop() {
     if(Serial.available()){
         input = Serial.readStringUntil('\n');
           Serial.print("You typed: " );
           Serial.println(input);
-      
+          
       int WordLength = input.length() + 1;  
       char WordArray[WordLength]; 
-
       input.toCharArray(WordArray, WordLength); 
 
       for(int i = 0; i < WordLength; i++)
@@ -24,4 +25,16 @@ void loop() {
       }
     
     }
+    arx::map<String, String> mp {
+      {"a", "100000"}, 
+      {"b", "110000"}
+    };
+  // range based access
+  for (const auto& m : mp)
+  {
+      Serial.print("{");
+      Serial.print(m.first); Serial.print(",");
+      Serial.print(m.second);
+      Serial.println("}");
+  }
 }
