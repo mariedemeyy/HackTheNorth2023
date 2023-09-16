4f01a9d95b8e5d026cfd1a4b3a22ee0400ff1f61
Marie De Mey <97183312+mariedemeyy@users.noreply.github.com>
2023-09-16 17:09:34 -0400

    Split printer into top, middle and bottom row

--- BraillePrinterCodeV1/BraillePrinterCodeV1.ino
+++ BraillePrinterCodeV1/BraillePrinterCodeV1.ino
@@ -1,6 +1,9 @@
 #include <ArxContainer.h>
 String input;
 
+
+// horse heart error there earth
+
 arx::map<char, String> mp {
   {'a', "100000"},
   {'b', "110000"},
@@ -51,32 +54,59 @@ void loop() {
 
       // Print each element of the character array OR
       // do something with the character array
-      for(int i = 0; i < WordLength - 1; i++)
+
+      // HARDCODE FOR 5 LETTER WORDS ONLY
+      char topRow[10]; 
+      char middleRow[10];
+      char bottomRow[10];
+      int leftCol;
+      int rightCol;
+      
+
+      // ISSUE WITH HOW IT PRINTS
+      // TRYING WITH ABE
+      // IT NEEDS TO PRINT E B A 
+      for(int i = WordLength - 1; i >= 0; i--)
       {
         // Get the Braille code for the letter in the array
-
         Serial.print("letter: ");
         Serial.println(WordArray[i]); 
-        // Serial.print("string: ");
-        // Serial.println(String(WordArray[i]));
-        Serial.print("try with char: ");
-        Serial.println(mp[WordArray[i]]);
-        // Serial.print("try with string");
-        // Serial.println(mp[String(WordArray[i])]);
 
-        // Serial.println("hello");
-        // for (const auto& m : mp)
-        // {
-        //   if(m.first == WordArray[i]) {
-        //     Serial.println("True");
-        //   }
-        //   Serial.println(mp["a"]);
-        //   // Serial.print("{");
-        //   // Serial.print(m.first); Serial.print(",");
-        //   // Serial.print(m.second);
-        //   // Serial.println("}");
-        // }
-        delay(1000);
+        String brailleCode = mp[WordArray[i]];
+        Serial.print("try with char: ");
+        Serial.println(brailleCode);
+       
+        for(int j = 0; j < WordLength - 1; j++) {
+        
+          leftCol = j * 2;
+          rightCol = (j * 2) + 1;
+
+          // Get 0 and 3 for topRow
+          topRow[leftCol] = brailleCode.charAt(3);
+          topRow[rightCol] = brailleCode.charAt(0);
+
+          // Get 1 and 4 for middleRow
+          middleRow[leftCol] = brailleCode.charAt(4);
+          middleRow[rightCol] = brailleCode.charAt(1);
+
+          // Get 2 and 5 for bottomRow
+          bottomRow[leftCol] = brailleCode.charAt(5);
+          bottomRow[rightCol] = brailleCode.charAt(2);
+
+        }
+      }
+      // MOTOR STUFF, USE THE ARRAYS
+      Serial.println("topRow");
+      for(int i = 0; i < 10; i++) {
+        Serial.print(topRow[i]);
+      }
+      Serial.println("middleRow");
+      for(int i = 0; i < 10; i++) {
+        Serial.print(middleRow[i]);
+      }
+      Serial.println("bottomRow");
+      for(int i = 0; i < 10; i++) {
+        Serial.print(bottomRow[i]);
       }
     }
     
