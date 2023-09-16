24b31c32abeb0a52c5f2558fc3822cbad72251f5
Marie De Mey <97183312+mariedemeyy@users.noreply.github.com>
2023-09-16 14:58:54 -0400

    Fiddling

--- BraillePrinterCodeV1/BraillePrinterCodeV1.ino
+++ BraillePrinterCodeV1/BraillePrinterCodeV1.ino
@@ -1,38 +1,37 @@
 #include <ArxContainer.h>
 String input;
 
-arx::map<String, String> mp {
-  {"a", "100000"},
-  {"b", "110000"},
-  {"c", "100100"},
-  {"d", "100110"},
-  {"e", "100010"},
-  {"f", "110100"},
-  {"g", "110110"},
-  {"h", "110010"},
-  {"i", "010100"},
-  {"j", "010110"},
-  {"k", "101000"},
-  {"l", "111000"},
-  {"m", "101100"},
-  {"n", "101110"},
-  {"o", "101000"},
-  {"p", "111100"},
-  {"q", "111110"},
-  {"r", "111010"},
-  {"s", "011100"},
-  {"t", "011110"},
-  {"u", "101001"},
-  {"v", "111001"},
-  {"w", "010111"},
-  {"x", "101101"},
-  {"y", "101111"},
-  {"z", "101011"}
+arx::map<char, String> mp {
+  {'a', "100000"},
+  {'b', "110000"},
+  {'c', "100100"},
+  {'d', "100110"},
+  {'e', "100010"},
+  {'f', "110100"},
+  {'g', "110110"},
+  {'h', "110010"},
+  {'i', "010100"}
+  // {'j', "010110"},
+  // {'k', "101000"},
+  // {'l', "111000"},
+  // {'m', "101100"},
+  // {'n', "101110"},
+  // {'o', "101000"},
+  // {'p', "111100"},
+  // {'q', "111110"},
+  // {'r', "111010"},
+  // {'s', "011100"},
+  // {'t', "011110"},
+  // {'u', "101001"},
+  // {'v', "111001"},
+  // {'w', "010111"},
+  // {'x', "101101"},
+  // {'y', "101111"},
+  // {'z', "101011"}
 };
 
 void setup() {
     Serial.begin(9600); 
-    delay(2000);  
  
     Serial.println("Type something!");
 }
@@ -58,9 +57,12 @@ void loop() {
 
         Serial.print("letter: ");
         Serial.println(WordArray[i]); 
-        Serial.print("braille code: ");
-        Serial.println(String(WordArray[i])); 
-        Serial.println(mp[String(WordArray[i])]);
+        // Serial.print("string: ");
+        // Serial.println(String(WordArray[i]));
+        Serial.print("try with char: ");
+        Serial.println(mp[WordArray[i]]);
+        // Serial.print("try with string");
+        // Serial.println(mp[String(WordArray[i])]);
 
         // Serial.println("hello");
         // for (const auto& m : mp)
@@ -74,7 +76,7 @@ void loop() {
         //   // Serial.print(m.second);
         //   // Serial.println("}");
         // }
-        
+        delay(1000);
       }
     }
     
